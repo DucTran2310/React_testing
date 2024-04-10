30eab25ca27a7fe3ff06684a85f948f5d5d1e71e
DucTran2310 <trananhduc23102000@gmail.com>
2024-04-10 09:11:14 +0700

    Update test msw

--- src/components/users/users.test.tsx
+++ src/components/users/users.test.tsx
@@ -1,32 +1,8 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import Users from './users';
-import { rest } from 'msw';
-import { SetupServer, setupServer } from 'msw/node';
-import Skills from '../skills/skills';
+import { render, screen } from '@testing-library/react';
 import Application from '../application/application';
+import Users from './users';
 
 describe('Users', () => {
-  let server: SetupServer;
-
-  beforeAll(() => {
-    server = setupServer(
-      rest.get('https://jsonplaceholder.typicode.com/users', (req, res, ctx) => {
-        return res(
-          //  ctx.status(200),
-          ctx.json([
-            { name: 'Bruce Wayne' },
-            { name: 'Clark Kent' },
-            { name: 'Princess Diana' },
-          ])
-        );
-      }),
-    );
-    server.listen();
-  });
-
-  afterEach(() => server.resetHandlers());
-
-  afterAll(() => server.close());
 
   test('renders correctly', () => {
     render(<Users />);
@@ -36,25 +12,9 @@ describe('Users', () => {
 
   test('should show Hello world', async () => {
     render(<Application />);
-    // Wait for the text "Hello world" to appear
     await (() => {
       expect(screen.getByText('Hello world')).toBeInTheDocument();
     });
   });
 
-  
-  test('renders error message when fetching fails', async () => {
-    // Mock server response to simulate a failed request
-    server.use(
-      rest.get('https://jsonplaceholder.typicode.com/users', (req, res, ctx) => {
-        return res(ctx.status(500)); // Simulate server error
-      })
-    );
-
-    render(<Users />);
-
-    // Assert that error message is rendered
-    const error = await screen.findByText('Error fetching users');
-    expect(error).toBeInTheDocument();
-  });
 });
